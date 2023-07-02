8633f60063eca8cd0f7a77f54d5c05c6a4397e78
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-07-02 14:46:43 +0100

    Initial Commit

---
+++ SwiftuiCustomComponents/SwiftuiCustomComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCustomComponentsApp.swift
+//  SwiftuiCustomComponents
+//
+//  Created by Zakarai Lachkar on 2/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCustomComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
